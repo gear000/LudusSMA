6b1e492e5b6a7d535cb16a336fe2dc38033bc24a
Riccardo <ricandreoni95@gmail.com>
2024-05-21 18:38:48 +0200

    AGGIUNTA POSIZIONE PARAMETRICA

--- lambda_functions/create_ig_stories/image_editing.py
+++ lambda_functions/create_ig_stories/image_editing.py
@@ -2,11 +2,15 @@ from PIL import Image, ImageFont, ImageDraw
 import requests
 import io
 
-LOWER_BOXEX_X0 = 50
-LOWER_BOXEX_Y0 = 860
-LOWER_BOXEX_X1 = 524
+UPPER_BOXES_START_POINT = (50, 50)
+UPPER_BOXES_WIDTH = 550
+UPPER_SECTIONS = ["title", "body"]
 
+LOWER_BOXES_START_POINT = (50, 860)
+LOWER_BOXES_WIDTH = 500
+LOWER_SECTIONS = ["date", "time", "location", "cost"]
 
+BOX_MARGIN = 5
 
 def draw_textboxes(my_image, edit_dict):
     """
@@ -21,22 +25,23 @@ def draw_textboxes(my_image, edit_dict):
     # x_1 = 524
     # y_1 = 524
     # lower_textbox = ((50, 860), (524,))
-    y_0 = LOWER_BOXEX_Y0
+    upper_y_0 = UPPER_BOXES_START_POINT[1]
+    lower_y_0 = LOWER_BOXES_START_POINT[1]
+
     for scope in edit_dict.keys():
         if edit_dict[scope]["text"] != "":
             if scope in ["title", "body"]:
-                image_editable.rectangle(edit_dict[scope]["textbox"], width=3)
+                upper_textbox = ((UPPER_BOXES_START_POINT[0], UPPER_BOXES_START_POINT[1]), 
+                                 (UPPER_BOXES_START_POINT[0]+UPPER_BOXES_WIDTH, upper_y_0+edit_dict[scope]["size"]))
+                image_editable.rectangle(upper_textbox, width=3)
+                upper_y_0 = upper_y_0+edit_dict[scope]["size"]
             else: 
 
-                lower_textbox = ((LOWER_BOXEX_X0, LOWER_BOXEX_Y0), 
-                                 (LOWER_BOXEX_X1, y_0+edit_dict[scope]["size"]))
+                lower_textbox = ((LOWER_BOXES_START_POINT[0], LOWER_BOXES_START_POINT[1]), 
+                                 (LOWER_BOXES_START_POINT[0]+LOWER_BOXES_WIDTH, lower_y_0+edit_dict[scope]["size"]))
                 image_editable.rectangle(lower_textbox, width=3)
-                y_0 = y_0+edit_dict[scope]["size"]
+                lower_y_0 = lower_y_0+edit_dict[scope]["size"]
     return my_image
-
-
-
-
 
 def get_google_font(font: str, size=24):
     """
@@ -45,7 +50,6 @@ def get_google_font(font: str, size=24):
     size: dimensione del font
     """
     url = f"https://github.com/google/fonts/blob/main/ofl/{font}?raw=true"
-    print(url)
     r = requests.get(url, allow_redirects=True)
     font = ImageFont.truetype(io.BytesIO(r.content), size=size)
 
@@ -78,6 +82,43 @@ def crop_image(my_image, aspect_ratio: tuple):
     # thumb.save(img_path_cropped)
     return image
 
+def get_text_size(max_width, text, font, max_size):
+    # Get appropriate font size to make the text fit the box in a single line
+    max_size = max_size # Start from default font size
+    text_length = get_google_font(font, max_size).getlength(text) # Get text length given font and size
+    # Finché il testo non ci sta nel box predefinito, riduco la size di 2 punti e riprovo
+    while text_length > max_width:
+        max_size -= 2
+        text_length = get_google_font(font, max_size).getlength(text)
+    actual_size = max_size
+    
+    return actual_size
+
+def get_text_position(textbox, anchor):
+    # Get textbox dimensions from coordinates
+    box_h = textbox[1][1] - textbox[0][1]
+
+    # Determinare la posizione del testo
+    # - se testo centrato ("anchor" == "mm"): posizione = centro del box
+    # - se testo allineato a dx ("anchor" == "lb"): posizione = angolo lower left del box
+    # - se testo allineato a dx ("anchor" == "lm"): posizione = middle left del box
+    # - per ora ci sono solo queste casistiche
+        
+    if anchor == "mm":
+        position_x = (textbox[1][0]+textbox[0][0])/2
+        position_y = (textbox[1][1]+textbox[0][1])/2
+        position = (position_x, position_y)
+    elif anchor == "lb":
+        position_x = textbox[0][0]
+        position_y = textbox[0][1] + box_h
+        position = (position_x, position_y)
+    elif anchor == "lm":
+        position_x = textbox[0][0]
+        position_y = textbox[0][1] + box_h/2
+        position = (position_x, position_y)
+
+    return position
+
 def write_on_image(my_image, edit_dict):
     """
     https://pillow.readthedocs.io/en/stable/handbook/text-anchors.html
@@ -85,44 +126,46 @@ def write_on_image(my_image, edit_dict):
 
     image_editable = ImageDraw.Draw(my_image)
 
+
+    upper_y_0 = UPPER_BOXES_START_POINT[1]
+    lower_y_0 = LOWER_BOXES_START_POINT[1]
+
     for scope in edit_dict.keys():
-        # Get textbox dimensions from coordinates
-        box_w = edit_dict[scope]["textbox"][1][0] - edit_dict[scope]["textbox"][0][0]
-        box_h = edit_dict[scope]["textbox"][1][1] - edit_dict[scope]["textbox"][0][1]
+        if edit_dict[scope]["text"] != "":
+            if scope in UPPER_SECTIONS:
+                actual_size = get_text_size(UPPER_BOXES_WIDTH, edit_dict[scope]["text"], edit_dict[scope]["font"], edit_dict[scope]["size"])
+                upper_textbox = ((UPPER_BOXES_START_POINT[0], upper_y_0), 
+                                 (UPPER_BOXES_START_POINT[0]+UPPER_BOXES_WIDTH, upper_y_0+actual_size)) 
+                position = get_text_position(upper_textbox, edit_dict[scope]["anchor"])      
 
-        # Get appropriate font size to make the text fit the box in a single line
-        max_size = edit_dict[scope]["size"] # Start from default font size
-        text_length = get_google_font(edit_dict[scope]["font"], max_size).getlength(edit_dict[scope]["text"]) # Get text length given font and size
-        # Finché il testo non ci sta nel box predefinito, riduco la size di 2 punti e riprovo
-        while text_length > box_w:
-            max_size -= 2
-            text_length = get_google_font(edit_dict[scope]["font"], max_size).getlength(edit_dict[scope]["text"])
+                # print(actual_size)        
+                image_editable.text(position, 
+                                    edit_dict[scope]["text"],
+                                    edit_dict[scope]["color"],
+                                    anchor=edit_dict[scope]["anchor"],
+                                    font=get_google_font(edit_dict[scope]["font"]
+                                                            , actual_size))
+                upper_y_0 = upper_y_0+actual_size+BOX_MARGIN
+                # print(scope, upper_y_0)
+            elif scope in LOWER_SECTIONS:
+                # print(scope)
+                actual_size = get_text_size(LOWER_BOXES_WIDTH, edit_dict[scope]["text"], edit_dict[scope]["font"], edit_dict[scope]["size"])
+                # print("actual_size", actual_size)  
+                lower_textbox = ((LOWER_BOXES_START_POINT[0], lower_y_0), 
+                                 (LOWER_BOXES_START_POINT[0]+LOWER_BOXES_WIDTH, lower_y_0+actual_size))
+                # print(lower_textbox)
+                position = get_text_position(lower_textbox, edit_dict[scope]["anchor"])   
 
-        # Determinare la posizione del testo
-        # - se testo centrato ("anchor" == "mm"): posizione = centro del box
-        # - se testo allineato a dx ("anchor" == "lb"): posizione = angolo lower left del box
-        # - se testo allineato a dx ("anchor" == "lm"): posizione = middle left del box
-        # - per ora ci sono solo queste casistiche
-            
-        if edit_dict[scope]["anchor"] == "mm":
-            position_x = (edit_dict[scope]["textbox"][1][0]+edit_dict[scope]["textbox"][0][0])/2
-            position_y = (edit_dict[scope]["textbox"][1][1]+edit_dict[scope]["textbox"][0][1])/2
-            position = (position_x, position_y)
-        elif edit_dict[scope]["anchor"] == "lb":
-            position_x = edit_dict[scope]["textbox"][0][0]
-            position_y = edit_dict[scope]["textbox"][0][1] + box_h
-            position = (position_x, position_y)
-        elif edit_dict[scope]["anchor"] == "lm":
-            position_x = edit_dict[scope]["textbox"][0][0]
-            position_y = edit_dict[scope]["textbox"][0][1] + box_h/2
-            position = (position_x, position_y)
-            
-        image_editable.text(position, 
-                            edit_dict[scope]["text"],
-                            edit_dict[scope]["color"],
-                            anchor=edit_dict[scope]["anchor"],
-                            font=get_google_font(edit_dict[scope]["font"]
-                                                    , max_size))
+                      
+                # print("position", position)   
+                # image_editable.rectangle(lower_textbox, width=3)
+                image_editable.text(position, 
+                                    edit_dict[scope]["text"],
+                                    edit_dict[scope]["color"],
+                                    anchor=edit_dict[scope]["anchor"],
+                                    font=get_google_font(edit_dict[scope]["font"]
+                                                            , actual_size))
+                lower_y_0 = lower_y_0+actual_size+BOX_MARGIN
     
     return my_image
 
@@ -137,9 +180,9 @@ def image_edit(img_path: str, edit_dict: dict, crop=True):
         # Carico immagine
         my_image = Image.open(img_path) 
 
-    # my_image = write_on_image(my_image, edit_dict)
-    my_image = draw_textboxes(my_image, edit_dict)
+    my_image = write_on_image(my_image, edit_dict)
+    # my_image = draw_textboxes(my_image, edit_dict)
 
     # Salvataggio risutlato
-    my_image.save("BOX_"+img_path)
+    my_image.save("EDIT_"+img_path)
 
