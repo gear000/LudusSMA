e63add4e7fa9d1a448f6336b1a6749bd3e0c85e2
Daniele Franzoni <d.franzoni.97@gmail.com>
2024-09-01 18:07:29 +0200

    commented vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,37 +4,37 @@ variable "aws_region" {
   description = "AWS Region"
 }
 
-variable "telegram_bot_key_parameter" {
-  type    = string
-  default = "/telegram/bot-token"
-}
+# variable "telegram_bot_key_parameter" {
+#   type    = string
+#   default = "/telegram/bot-token"
+# }
 
-variable "telegram_allow_chat_ids_key_parameter" {
-  type    = string
-  default = "/telegram/allow-chat-ids"
-}
+# variable "telegram_allow_chat_ids_key_parameter" {
+#   type    = string
+#   default = "/telegram/allow-chat-ids"
+# }
 
-variable "telegram_header_webhook_token_key_parameter" {
-  type    = string
-  default = "/telegram/header-webhook-token"
-}
+# variable "telegram_header_webhook_token_key_parameter" {
+#   type    = string
+#   default = "/telegram/header-webhook-token"
+# }
 
-variable "meta_client_secret_key_parameter" {
-  type    = string
-  default = "/meta/client-secret"
-}
+# variable "meta_client_secret_key_parameter" {
+#   type    = string
+#   default = "/meta/client-secret"
+# }
 
-variable "meta_access_token_key_parameter" {
-  type    = string
-  default = "/meta/access-token"
-}
+# variable "meta_access_token_key_parameter" {
+#   type    = string
+#   default = "/meta/access-token"
+# }
 
-variable "sqs_telegram_updates_name" {
-  type    = string
-  default = "TelegramUpdates"
-}
+# variable "sqs_telegram_updates_name" {
+#   type    = string
+#   default = "TelegramUpdates"
+# }
 
-variable "bedrock_models_region" {
-  type    = string
-  default = "us-west-2"
-}
+# variable "bedrock_models_region" {
+#   type    = string
+#   default = "us-west-2"
+# }
