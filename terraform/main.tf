abd2d3b41775478668b28e9c8b01b3b6d67d3ed1
Daniele Franzoni <d.franzoni.97@gmail.com>
2024-09-30 23:13:52 +0200

    Fixed iam policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -181,12 +181,21 @@ data "aws_iam_policy_document" "lambda_rotate_tokens_policy_document" {
   statement {
     effect = "Allow"
     actions = [
-      "ssm:GetParameter",
       "ssm:PutParameter"
     ]
     resources = [
       "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${var.telegram_header_webhook_token_key_parameter}",
       "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${var.meta_access_token_key_parameter}"
+    ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "ssm:GetParameter",
+    ]
+    resources = [
+      "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/telegram/*",
+      "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/meta/*"
     ]
   }
 }
